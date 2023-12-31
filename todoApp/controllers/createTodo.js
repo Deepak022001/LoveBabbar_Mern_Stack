b04b5bd118777439f9f8c6e8363cb4add6d8290f
Dipu <deepak@gmail.com>
2023-12-31 15:08:52 +0530

    lecture 2 completed

--- todoApp/controllers/createTodo.js
+++ todoApp/controllers/createTodo.js
@@ -1,31 +1,30 @@
-//import th model
-const Todo = require("../models/Todo");
+// import model
+const Todo=require('../models/Todo');
 
-//define route handler
-
-exports.createTodo = async(req,res) => {
-    try {
-            //extract title and desxcription from reauest body
-            const {title,description} = req.body;
-            //create a new Todo Obj and insert in DB
-            const response = await Todo.create({title,description});
-            //send a json response with a success flag
-            res.status(200).json(
-                {
-                    success:true,
-                    data:response,
-                    message:'Entry Created Successfully'
-                }
-            );
+// define route handler
+exports.createTodo=async(req,res)=>{
+    try{
+        // extrach titla and description from request body
+        const {title,description}=req.body;
+        // create a new todo obj and insert in db
+        const response=await Todo.create({title,description});
+        // send a json response with a success flag
+        res.status(200).json(
+            {
+                success:true,
+                data:response,
+                message:'Entry Created Successfully'
+            }
+        );
     }
-    catch(err) {
-        console.error(err);
-        console.log(err);
+    catch(error){
+        console.error(error);
+        console.log(error);
         res.status(500)
         .json({
             success:false,
             data:"internal server error",
-            message:err.message,
+            message:err.message
         })
     }
 }
--- todoApp/index.js
+++ todoApp/index.js
@@ -1,6 +1,29 @@
 const express=require('express');
 const app=express();
 
-app.listen(3000,()=>{
-    console.log("App is running successfully");
+// app.listen(3000,()=>{
+//     console.log("App is running successfully");
+// })
+
+require("dotenv").config();
+const PORT=process.env.PORT||4000;
+
+app.use(express.json());
+
+
+// import routes for todo apis
+
+const todoRoutes=require('./routes/todos')
+
+app.use("/api/v1",todoRoutes);
+
+
+app.listen(PORT,()=>{
+    console.log(`Server started succuseffully at ${PORT}`)
+})
+
+const dbConnect=require("./config/database");
+
+app.get("/",(req,res)=>{
+    res.send(`<h1>This is a HOMEPAGE baby </h1>`);
 })
